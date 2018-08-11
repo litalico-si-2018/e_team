567cb83cbaed9e2fc976571de9a06a247cae6939
Nkowne63 <neutron63zf@gmail.com>
2018-08-11 18:10:33 +0900

    rebuild: model

--- db/migrate/20180811082322_create_articles.rb
+++ db/migrate/20180811082322_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      t.string :articleid
       t.datetime :date
       t.string :body
       t.boolean :isread
