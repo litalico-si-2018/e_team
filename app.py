3dd5c48e9a914b50bea51150a2744ba97469231d
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-08-11 23:05:56 +0900

    adminとユーザを分けて表示

--- app.py
+++ app.py
@@ -35,34 +35,29 @@ def show_users():
     all_user = User_table.query.filter(User_table.id >= 2)
     return render_template("all_user.html",all_user = all_user)
 
+@app.route("/articles/1")
+def supported_posting():
+    # adminのデータを持ってくる
+    return render_template("article.html")
+
 # rooting of post
-@app.route("/posting_page")
-def top_page():
-    if current_user.is_authenticated:
-        return render_template("posting_page.html")
-    else:
-        return redirect("/")
+@app.route("/admin/articles")
+def supporter_posting():
+    # ユーザーのデータを持ってくる
+    return render_template("article.html")
 
 # end post rooting
 
 # rooting of past posted
 @app.route("/posted_page",methods=["POST"])
 def post_page():
-    title = request.form["title"]
     contents = request.form["contents"]
-    file = request.files["image"]
-    file_path = "static/post_img/" + secure_filename(file.filename)
     file.save(file_path)
-    new_page = Diary_table(id=current_user.id,title=title, contents=contents,file_path=file_path)
-    db.session.add(new_page)
+    # supporter/suppportedのidを取ってくる
+    new_article = Message_table(contents=contents)
+    db.session.add(new_article)
     db.session.commit()
     return redirect("/")
-
-@app.route("/detailed_past_post/<int:diary_id>")
-def detailed_past_post(diary_id):
-    page = Diary_table.query.filter(Diary_table.diary_id == diary_id).first()
-    return render_template("detailed_page.html",page=page)
-
 
 # the end of past posted
 
