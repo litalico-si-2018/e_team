deb558c3bbd8c2f0002e15b1ad8f521c556a62af
Nkowne63 <neutron63zf@gmail.com>
2018-08-11 17:58:19 +0900

    add: routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/users/:name/articles', to: 'articles#show'
+  get '/users/:name/articles/:date', to: 'articles#show'
+  get '/users/:name/admin/articles', to: 'articles#show'
+  get '/users/:name/admin/articles/:date', to: 'articles#show'
+  get '/admins/:name', to: 'users#index'
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
