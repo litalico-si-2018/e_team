891d43088704e8e4dc4329c31a43efea8d760e7c
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-08-12 15:01:23 +0900

    final

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def show_users():
 @app.route("/admin/articles/<int:id>")
 def supporter_posting(id):
     if Message_table.query.filter(Message_table.sender_id==2):
-        from_message = Message_table.query.filter(Message_table.sender_id==2)
+        from_message = sorted(Message_table.query.filter(Message_table.sender_id==2),key = lambda message:message.diary_id)
         username = "test"
         return render_template("re_article.html",username=username,from_message=from_message)
     else:
@@ -53,7 +53,7 @@ def post_page():
     new_article = Message_table(sender_id=sender_id, receiver_id = receiver_id,contents=contents,attention=0)
     db.session.add(new_article)
     db.session.commit()
-    return redirect("/articles/new")
+    return redirect("/articles/new#form")
 
 @app.route("/articles/new")
 def supported_posting():
@@ -67,7 +67,7 @@ def attention():
     update_attention = Message_table.query.filter(Message_table.diary_id == diary_id).first()
     update_attention.attention = 1
     db.session.commit()
-    return redirect("/admin/users")
+    return redirect("/admin/articles/2#diary-" + diary_id)
 
 #DBのコマンド
 @app.cli.command("initdb")
